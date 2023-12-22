dd81b75b4eadb0fa6dddd571bac551df7f3cad90
Nikolas-Bars~ <kasp2409@mail.ru>
2023-12-22 18:10:25 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -31,57 +31,13 @@ app.get('/', (req, res) => {
     res.json('Hi mf')
 })
 
-app.get('/courses', (req, res) => {
+app.get('/courses', (req: Request, res: Response) => {
     let foundedCourses = db.courses
     if(req.query.title) {
-        foundedCourses = foundedCourses.filter((el) => el.title.indexOf(req.query.title) > -1)
+        foundedCourses = foundedCourses.filter((el) => el.title.indexOf(req.query.title as string) > -1)
     }
 
     res.json(foundedCourses)
-})
-
-app.get('/courses/:id', (req, res) => {
-    const foundedCourse = db.courses.find((el) => el.id === +req.params.id)
-    if (!foundedCourse) {
-        res.sendStatus(HTTP_STATUSE.NOT_FOUND_404)
-    }
-    res.json(foundedCourse)
-})
-
-app.post('/courses', (req, res) => {
-    if (!req.body.title) {
-        res.sendStatus(HTTP_STATUSE.BAD_REQUEST_400)
-        return
-    }
-    const newCourse = {
-        id: +(new Date()),
-        title: req.body.title
-    }
-    db.courses.push(newCourse)
-
-    res.status(HTTP_STATUSE.CREATED_201).json(newCourse)
-})
-
-app.delete('/courses/:id', (req, res) => {
-    db.courses = db.courses.filter((el) => el.id !== +req.params.id)
-
-    res.sendStatus(204)
-})
-
-app.put('/courses/:id', (req, res) => {
-    if (!req.body.title) {
-        res.sendStatus(HTTP_STATUSE.BAD_REQUEST_400)
-        return
-    }
-    const foundedCourse = db.courses.find((el) => el.id === +req.params.id)
-
-    if (!foundedCourse) {
-        res.sendStatus(HTTP_STATUSE.NOT_FOUND_404)
-    }
-
-    foundedCourse.title = req.body.title
-
-    res.sendStatus(HTTP_STATUSE.NO_CONTENT_204)
 })
 
 app.listen(port, () => {
